6f41308d49022dfa862cec86f177418b2c519463
julfoi <julfoi@accountr.eu>
2011-05-25 14:29:19 +0200

    Modified database
    Change-Id: I261421efb84c60bda969a6b6cee73449471e82ef

--- scripts/data.sqlite.sql
+++ scripts/data.sqlite.sql
@@ -2,7 +2,7 @@
 --
 -- You can begin populating the database with the following SQL statements.
  
-INSERT INTO monster (name, hero, health, mana, attackDamage, defense, experience) VALUES
+INSERT INTO player (name, hero, health, mana, attackDamage, defense, experience) VALUES
 	('defaultPlayer',
 	'Warrior',
 	500,
