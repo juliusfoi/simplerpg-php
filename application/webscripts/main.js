18f62f71567e3a7cbec18970ce31ed8e8ce1b194
juliusfoi <jul.foi@accountr.eu>
2011-05-28 00:22:52 +0200

    Change-Id: I98d67398bc9d9f519b159cdb1d8ec898bde49623

--- application/webscripts/main.js
+++ application/webscripts/main.js
@@ -1,5 +1,7 @@
 function IrontouchGameSetup(){
 	$('#loadingBattle').hide();
+	$('.locationPicture').css('width', '200px');
+	$('.locationPicture').css('height', 'auto');
 	$('.attack-button').live('click', function(){
 		loadingBattle();
 		var monsterId = $(this).attr('id');
