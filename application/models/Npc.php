38706dcbece0e57c421cc930ab0e3a709474b725
juliusfoi <jul.foi@accountr.eu>
2011-06-11 23:29:20 +0200

    Change-Id: I7e06ba957d07e9bb4d4091a482ec1a1f669ef62f

--- application/models/Npc.php
+++ application/models/Npc.php
@@ -6,7 +6,71 @@ class Application_Model_Npc
 	protected $_name;
 	protected $_quest;
 	protected $_talk;
+	
 
-
+	public function __construct(array $options = null)
+    {
+        if (is_array($options)) {
+            $this->setOptions($options);
+        }
+    }
+ 
+    public function __set($name, $value)
+    {
+        $method = 'set' . $name;
+        if (('mapper' == $name) || !method_exists($this, $method)) {
+            throw new Exception('Invalid player property');
+        }
+        $this->$method($value);
+    }
+ 
+    public function __get($name)
+    {
+        $method = 'get' . $name;
+        if (('mapper' == $name) || !method_exists($this, $method)) {
+            throw new Exception('Invalid player property');
+        }
+        return $this->$method();
+    }
+ 
+    public function setOptions(array $options)
+    {
+        $methods = get_class_methods($this);
+        foreach ($options as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (in_array($method, $methods)) {
+                $this->$method($value);
+            }
+        }
+        return $this;
+    }
+    
+    public function getId()
+    {
+    	if($this->_id == null || $this->_id == '')
+    		return null;
+    	else
+    		return $this->_id;
+    }
+    
+    public function setId($id)
+    {
+    	$this->_id = (int) $id;
+    	return $this;
+    }
+    
+    public function getName()
+    {
+    	if($this->_name == null || $this->_name == '')
+    		return null;
+    	else
+    		return $this->_name;
+    }
+    
+    public function setName($name)
+    {
+    	$this->_name = (string) $name;
+    	return $this;
+    }
 }
 
